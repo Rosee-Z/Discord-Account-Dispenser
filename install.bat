b71e12688929289b57005d377083296e9e31d317
Rosee <78645652+Rosee-Z@users.noreply.github.com>
2021-06-19 21:29:14 +0100

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,9 @@
+color 2
+pip install typing
+pip install pip install colorama
+pip install discord
+pip install asyncio
+pip install aiohttp
+pip install requests
+color 9
+echo finished installing you can now run the main python file
