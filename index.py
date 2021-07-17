c3e73fa66b1278bf11b821918563d00b73c65bc0
Rosee <78645652+Rosee-Z@users.noreply.github.com>
2021-07-17 12:03:46 +0100

    Update index.py

--- index.py
+++ index.py
@@ -17,7 +17,6 @@ client = commands.Bot(command_prefix='!')
 client.remove_command('help')
 os.system('cls')
 serverid = 838789505888419841 #replace this with your own server id braindead paster
-rainbowrolename = "bot" # this is a rainbow role thing so it changes color every couple seconds making it look cool change if you want xx
 delay = 2
 colours = [discord.Color.dark_orange(),discord.Color.orange(),discord.Color.dark_gold(),discord.Color.gold(),discord.Color.dark_magenta(),discord.Color.magenta(),discord.Color.red(),discord.Color.dark_red(),discord.Color.blue(),discord.Color.dark_blue(),discord.Color.teal(),discord.Color.dark_teal(),discord.Color.green(),discord.Color.dark_green(),discord.Color.purple(),discord.Color.dark_purple()]
 
@@ -30,7 +29,6 @@ async def on_ready():
 
 @client.event
 async def on_ready():
-    client.loop.create_task(lolhaha(rainbowrolename))
     banner = f"""
         {Fore.RED};) ██╗  █{Fore.YELLOW}█╗███████{Fore.GREEN}╗███╗   █{Fore.CYAN}█╗███████{Fore.BLUE}█╗ █████╗{Fore.MAGENTA} ██╗ :-).
         {Fore.RED};) ██║  █{Fore.YELLOW}█║██╔════{Fore.GREEN}╝████╗  █{Fore.CYAN}█║╚══██╔═{Fore.BLUE}═╝██╔══██{Fore.MAGENTA}╗██║ :-).
@@ -425,30 +423,5 @@ async def unban(ctx, *, member):
             await ctx.guild.unban(user)
             await ctx.send(f'Unbanned {user.mention}')
             return
-
-async def lolhaha(role):
-    for role in client.get_guild(serverid).roles:
-        if str(role) == str(rainbowrolename):
-            print("detected role")
-            while not client.is_closed():
-                try:
-                    await role.edit(color=random.choice(colours))
-                except Exception:
-                    print("can't edit role, make sure the bot role is above the rainbow role and that is have the perms to edit roles")
-                    pass
-                await asyncio.sleep(delay)
-    print('role with the name "' + rainbowrolename +'" not found')
-    print("creating the role...")
-    try:
-        await client.get_guild(serverid).create_role(reason="Created rainbow role", name=rainbowrolename)
-        print("role created!")
-        await asyncio.sleep(2)
-        client.loop.create_task(lolhaha(rainbowrolename))
-    except Exception as e:
-        print("couldn't create the role. Make sure the bot have the perms to edit roles")
-        print(e)
-        pass
-        await asyncio.sleep(10)
-        client.loop.create_task(lolhaha(rainbowrolename))
 
 client.run(token.strip())
